ae1341f9863c2fd7245b477b6bc174d69a0a6fda
falto <sb051380us@gmail.com>
2023-12-04 02:37:54 +0900

    stable

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,14 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series16 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series17 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series18 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
@@ -45,90 +45,106 @@
             this.button2 = new System.Windows.Forms.Button();
             this.yLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea5.AxisX.IntervalOffset = 0.5D;
-            chartArea5.AxisX.IsMarginVisible = false;
-            chartArea5.AxisX.IsReversed = true;
-            chartArea5.AxisX.LabelStyle.Enabled = false;
-            chartArea5.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
-            chartArea5.AxisX.ScrollBar.Enabled = false;
-            chartArea5.AxisY.IsStartedFromZero = false;
-            chartArea5.AxisY.LabelStyle.Format = "N0";
-            chartArea5.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
-            chartArea5.AxisY.ScrollBar.Enabled = false;
-            chartArea5.AxisY2.LabelStyle.Format = "N0";
-            chartArea5.AxisY2.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
-            chartArea5.AxisY2.ScrollBar.Enabled = false;
-            chartArea5.CursorX.AutoScroll = false;
-            chartArea5.CursorX.IntervalOffset = 0.5D;
-            chartArea5.CursorX.IsUserSelectionEnabled = true;
-            chartArea5.Name = "p";
-            chartArea6.AlignWithChartArea = "p";
-            chartArea6.AxisX.IntervalOffset = 0.5D;
-            chartArea6.AxisX.IsMarginVisible = false;
-            chartArea6.AxisX.IsReversed = true;
-            chartArea6.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
-            chartArea6.AxisX.ScrollBar.Enabled = false;
-            chartArea6.AxisY.LabelStyle.Format = "#,##0,K";
-            chartArea6.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
-            chartArea6.AxisY.ScrollBar.Enabled = false;
-            chartArea6.CursorX.AutoScroll = false;
-            chartArea6.CursorX.IntervalOffset = 0.5D;
-            chartArea6.CursorX.IsUserSelectionEnabled = true;
-            chartArea6.Name = "v";
-            this.chart1.ChartAreas.Add(chartArea5);
-            this.chart1.ChartAreas.Add(chartArea6);
+            chartArea1.AxisX.IntervalOffset = 0.5D;
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.AxisX.IsReversed = true;
+            chartArea1.AxisX.LabelStyle.Enabled = false;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisX.MajorTickMark.Enabled = false;
+            chartArea1.AxisX.ScrollBar.Enabled = false;
+            chartArea1.AxisX2.IsReversed = true;
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.AxisY.LabelStyle.Format = "N0";
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisY.ScrollBar.Enabled = false;
+            chartArea1.AxisY2.LabelStyle.Format = "N0";
+            chartArea1.AxisY2.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisY2.ScrollBar.Enabled = false;
+            chartArea1.CursorX.AutoScroll = false;
+            chartArea1.CursorX.IntervalOffset = 0.5D;
+            chartArea1.CursorX.IsUserSelectionEnabled = true;
+            chartArea1.InnerPlotPosition.Auto = false;
+            chartArea1.InnerPlotPosition.Height = 99F;
+            chartArea1.InnerPlotPosition.Width = 90F;
+            chartArea1.Name = "p";
+            chartArea1.Position.Auto = false;
+            chartArea1.Position.Height = 75F;
+            chartArea1.Position.Width = 100F;
+            chartArea1.Position.Y = 3F;
+            chartArea2.AlignWithChartArea = "p";
+            chartArea2.AxisX.IntervalOffset = 0.5D;
+            chartArea2.AxisX.IsMarginVisible = false;
+            chartArea2.AxisX.IsReversed = true;
+            chartArea2.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea2.AxisX.ScrollBar.Enabled = false;
+            chartArea2.AxisY.LabelStyle.Format = "#,##0,K";
+            chartArea2.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea2.AxisY.ScrollBar.Enabled = false;
+            chartArea2.CursorX.AutoScroll = false;
+            chartArea2.CursorX.IntervalOffset = 0.5D;
+            chartArea2.CursorX.IsUserSelectionEnabled = true;
+            chartArea2.InnerPlotPosition.Auto = false;
+            chartArea2.InnerPlotPosition.Height = 78F;
+            chartArea2.InnerPlotPosition.Width = 90F;
+            chartArea2.Name = "v";
+            chartArea2.Position.Auto = false;
+            chartArea2.Position.Height = 20F;
+            chartArea2.Position.Width = 100F;
+            chartArea2.Position.Y = 80F;
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ChartAreas.Add(chartArea2);
             this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.chart1.Location = new System.Drawing.Point(3, 25);
             this.chart1.Name = "chart1";
-            series13.ChartArea = "p";
-            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series13.Color = System.Drawing.Color.Red;
-            series13.Name = "5";
-            series13.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
-            series14.BorderWidth = 2;
-            series14.ChartArea = "p";
-            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series14.Color = System.Drawing.Color.Gold;
-            series14.Name = "20";
-            series15.BorderWidth = 2;
-            series15.ChartArea = "p";
-            series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series15.Color = System.Drawing.Color.Green;
-            series15.Name = "60";
-            series16.ChartArea = "p";
-            series16.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series16.Color = System.Drawing.Color.Gray;
-            series16.Name = "120";
-            series17.ChartArea = "p";
-            series17.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
-            series17.Color = System.Drawing.Color.Blue;
-            series17.CustomProperties = "PriceDownColor=Blue, PriceUpColor=Red";
-            series17.Name = "p";
-            series17.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
-            series17.YValuesPerPoint = 4;
-            series17.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
-            series18.ChartArea = "v";
-            series18.Color = System.Drawing.Color.Blue;
-            series18.Name = "v";
-            this.chart1.Series.Add(series13);
-            this.chart1.Series.Add(series14);
-            this.chart1.Series.Add(series15);
-            this.chart1.Series.Add(series16);
-            this.chart1.Series.Add(series17);
-            this.chart1.Series.Add(series18);
+            series1.ChartArea = "p";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Name = "5";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            series2.BorderWidth = 2;
+            series2.ChartArea = "p";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Gold;
+            series2.Name = "20";
+            series3.BorderWidth = 2;
+            series3.ChartArea = "p";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Color = System.Drawing.Color.Green;
+            series3.Name = "60";
+            series4.ChartArea = "p";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Color = System.Drawing.Color.Gray;
+            series4.Name = "120";
+            series5.ChartArea = "p";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
+            series5.Color = System.Drawing.Color.Blue;
+            series5.CustomProperties = "PriceDownColor=Blue, PriceUpColor=Red";
+            series5.Name = "p";
+            series5.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            series5.YValuesPerPoint = 4;
+            series5.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series6.ChartArea = "v";
+            series6.Color = System.Drawing.Color.Blue;
+            series6.Name = "v";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
             this.chart1.Size = new System.Drawing.Size(869, 570);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -225,28 +241,6 @@
             this.tableLayoutPanel1.Size = new System.Drawing.Size(875, 654);
             this.tableLayoutPanel1.TabIndex = 7;
             // 
-            // panel1
-            // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(767, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(108, 22);
-            this.panel1.TabIndex = 8;
-            // 
-            // panel2
-            // 
-            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.Controls.Add(this.trackBar1);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Location = new System.Drawing.Point(698, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(168, 44);
-            this.panel2.TabIndex = 8;
-            // 
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 2;
@@ -262,6 +256,28 @@
             this.tableLayoutPanel2.Size = new System.Drawing.Size(869, 50);
             this.tableLayoutPanel2.TabIndex = 9;
             // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.trackBar1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(698, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(168, 44);
+            this.panel2.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(767, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(108, 22);
+            this.panel1.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
@@ -275,11 +291,11 @@
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
