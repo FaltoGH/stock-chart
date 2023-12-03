db8804b35659a9e1e5a4bbd6323ac10b6b674e25
falto <sb051380us@gmail.com>
2023-12-03 21:08:28 +0900

    stable.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
@@ -39,6 +43,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
@@ -47,10 +52,13 @@
             // 
             chartArea1.AxisX.IntervalOffset = 0.5D;
             chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.AxisX.IsReversed = true;
             chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
             chartArea1.AxisX.ScrollBar.Enabled = false;
-            chartArea1.AxisY.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
-            chartArea1.AxisY2.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.True;
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.AxisY.LabelStyle.Format = "N0";
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
+            chartArea1.AxisY.ScrollBar.Enabled = false;
             chartArea1.AxisY2.LabelStyle.Format = "N0";
             chartArea1.AxisY2.MajorGrid.LineColor = System.Drawing.Color.Gainsboro;
             chartArea1.AxisY2.ScrollBar.Enabled = false;
@@ -65,22 +73,41 @@
             this.chart1.Location = new System.Drawing.Point(23, 59);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
-            series1.CustomProperties = "PriceDownColor=Blue, PriceUpColor=Red";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Red;
             series1.Legend = "Legend1";
-            series1.Name = "Series1";
+            series1.Name = "5";
             series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
-            series1.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
-            series1.YValuesPerPoint = 4;
-            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series2.BorderWidth = 2;
             series2.ChartArea = "ChartArea1";
             series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Gold;
             series2.Legend = "Legend1";
-            series2.Name = "Series2";
-            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
-            series2.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            series2.Name = "20";
+            series3.BorderWidth = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Color = System.Drawing.Color.Green;
+            series3.Legend = "Legend1";
+            series3.Name = "60";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Color = System.Drawing.Color.Gray;
+            series4.Legend = "Legend1";
+            series4.Name = "120";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
+            series5.CustomProperties = "PriceDownColor=Blue, PriceUpColor=Red";
+            series5.Legend = "Legend1";
+            series5.Name = "p";
+            series5.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.String;
+            series5.YValuesPerPoint = 4;
+            series5.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
             this.chart1.Series.Add(series1);
             this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
             this.chart1.Size = new System.Drawing.Size(776, 417);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -113,20 +140,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(694, 32);
+            this.textBox1.Location = new System.Drawing.Point(702, 32);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(52, 21);
             this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0";
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(749, 36);
+            this.label1.Location = new System.Drawing.Point(757, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(17, 12);
             this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.label1.Text = "/0";
             // 
             // button1
             // 
@@ -146,11 +174,16 @@
             this.button2.Text = "+";
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(840, 577);
+            this.ClientSize = new System.Drawing.Size(842, 595);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -158,6 +191,7 @@
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.hScrollBar1);
             this.Controls.Add(this.chart1);
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
@@ -176,5 +210,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,75 +16,139 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        const byte H = 0;
+        const byte L = 1;
+        const byte O = 2;
+        const byte C = 3;
         public Form1()
         {
             InitializeComponent();
+        }
 
-            AddPoint("20230001", 4500, 500, 4300, 4400);
-            for (int i = 15000; i < 15300; i++)
+        public void AddAllPointsByFile(string csvfilename)
+        {
+            Clear();
+            string[] lines = File.ReadAllLines(csvfilename);
+            foreach(string line in lines)
             {
-                AddPoint("324" + i, 9+i, 4+i, 5+i, 6+i);
-                AddPoint("999" + i, 9+i*2, 4+i*2, 6+i*2, 5+i*2);
+                string[] tokens = line.Split();
+                if(tokens.Length >= 5)
+                {
+                    if (tokens[0].Length > 0)
+                        AddPoint(tokens[0], Parse(tokens[2]), Parse(tokens[3]), Parse(tokens[1]), Parse(tokens[4]));
+                }
             }
-            AddPoint("20271231", 3200, 400, 1500, 400);
             EndAddingPoints();
         }
-        void Assert(bool x, string msg=null)
+
+        public void Clear()
+        {
+            foreach(var a in Series)
+                a.Points.Clear();
+        }
+
+        static int Parse(string s)
+        {
+            return int.Parse(s, NumberStyles.AllowThousands);
+        }
+
+
+        static void Assert(bool x, string msg=null)
         {
             if (!x)
                 throw new Exception("Assertion failed: " + msg);
         }
-        Series Series => chart1.Series[0];
-        DataPointCollection Points => Series.Points;
+
+        SeriesCollection Series => chart1.Series;
+        Series PSeries => Series["p"];
+        DataPointCollection PPoints => PSeries.Points;
         ChartArea ChartArea => chart1.ChartAreas[0];
         Axis AxisX => ChartArea.AxisX;
         AxisScaleView XView => AxisX.ScaleView;
-        double AxisXMin => AxisX.Minimum;
-        double AxisXMax => AxisX.Maximum;
+        double AxisXMin
+        {
+            get
+            {
+                if (double.IsNaN(AxisX.Minimum))
+                    return 0;
+                return AxisX.Minimum;
+            }
+            set => AxisX.Minimum = value;
+        }
 
+        double AxisXMax
+        {
+            get
+            {
+                if (double.IsNaN(AxisX.Maximum))
+                    return 0;
+                return AxisX.Maximum;
+            }
+            set => AxisX.Maximum = value;
+        }
+
+        double AxisXSize => AxisXMax - AxisXMin;
         double XViewPos
         {
             get
             {
                 if (double.IsNaN(XView.Position))
-                    return 0.5;
+                    return AxisXMin;
                 return XView.Position;
             }
             set
             {
-                Assert(value >= AxisXMin, $"XViewPos: value({value})>=AxisXMin({AxisXMin})");
-                Assert(value + XViewSize <= AxisXMax, $"XViewPos: value({value})+XViewSize({XViewSize})<=AxisXMax({AxisXMax})");
-                hScrollBar1.Value = (int)value;
-                XView.Position = value;
-                UpdateY2View();
+                SetXViewPosSize(value, XViewSize);
             }
         }
 
-        int PointsCount => Points.Count;
+        void SetXViewPosSize(double xviewpos, int xviewsize)
+        {
+            Assert(xviewpos >= AxisXMin);
+            Assert(xviewpos + xviewsize <= AxisXMax);
+            XView.Position = xviewpos;
+            XView.Size = xviewsize;
+
+            UpdateYView();
+            UpdateScrollBar();
+            UpdateTrackBar();
+            UpdateTextBox();
+        }
+        
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            if (e.KeyCode == Keys.F12)
+            {
+                AddAllPointsByFile("LG생활건강.txt");
+            }
+            else if (e.KeyCode == Keys.F11)
+            {
+                AddAllPointsByFile("엔에이치스팩30.txt");
+            }
+        }
+
+
+
+        int PPointsCount => PPoints.Count;
 
         int XViewSize
         {
             get
             {
                 if (double.IsNaN(XView.Size))
-                    return Points.Count;
+                    return PPoints.Count;
                 return (int)XView.Size;
             }
             set
             {
-                Assert(XViewPos + value <= AxisXMax, $"XViewSize: XViewPos({XViewPos})+value({value})<=AxisMax({AxisXMax})");
-                trackBar1.Value = value;
-                toolTip.SetToolTip(trackBar1, value.ToString());
-                hScrollBar1.LargeChange = value;
-                textBox1.Text = value.ToString();
-                XView.Size = value;
-                UpdateTextBox();
-                UpdateY2View();
+                SetXViewPosSize(XViewPos, value);
             }
         }
 
         /// <summary>
-        /// Add point. First date first in.
+        /// Add point. Last date first in.
         /// </summary>
         public void AddPoint(string date, int h, int l, int o, int c)
         {
@@ -92,65 +158,102 @@ namespace WindowsFormsApp1
             Assert(l <= o);
             Assert(l <= c);
             Assert(l > 0);
-
-            int idx = Points.AddXY(date, h, l, o, c);
-            DataPoint point = Points[idx];
+            int idx = PPoints.AddXY(date, h, l, o, c);
+            DataPoint point = PPoints[idx];
             point.Color = c > o ? Color.Red : Color.Blue;
         }
 
+        /// <summary>
+        /// Call me when all points were added.
+        /// </summary>
         public void EndAddingPoints()
         {
-            AxisX.Minimum = 0.5;
-            AxisX.Maximum = PointsCount + 0.5;
-            hScrollBar1.Maximum = PointsCount - 1;
-            hScrollBar1.LargeChange = trackBar1.Value = trackBar1.Maximum = PointsCount;
-            label1.Text = $"/{PointsCount}";
+            hScrollBar1.Maximum = PPointsCount - 1;
+            hScrollBar1.LargeChange = trackBar1.Value = trackBar1.Maximum = PPointsCount;
+            AxisXMin = 0.5;
+            AxisXMax = PPointsCount + 0.5;
+            SetXViewPosSize(0.5, PPoints.Count);
+            label1.Text = $"/{PPointsCount}";
             UpdateTextBox();
-            chart1.Series[1].Points.MovingAverage(Points, 10);
+            MovingAverage(Series["5"].Points, PPoints, 5);
+            MovingAverage(Series["20"].Points, PPoints, 20);
+            MovingAverage(Series["60"].Points, PPoints, 60);
+            MovingAverage(Series["120"].Points, PPoints, 120);
+            UpdateYView();
+            SetXViewSizeSafely(preferredXViewSize);
         }
+
+        static void MovingAverage(DataPointCollection points, DataPointCollection target, int n)
+        {
+            for (int i = n - 1; i < target.Count; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += target[i - j].YValues[C];
+                }
+                points.AddXY(target[i].XValue, sum / n);
+            }
+        }
+
 
         void UpdateTextBox()
         {
             textBox1.Text = XViewSize.ToString();
         }
 
+
+
+        //---
+        // Value = AxisXSize - XViewPos - XViewSize + 0.5
+        // LargeChange = XViewSize;
+        void UpdateScrollBar()
+        {
+            hScrollBar1.Value = (int)(AxisXSize - XViewPos - XViewSize + 0.5);
+            hScrollBar1.LargeChange = XViewSize;
+        }
+        // XViewPos = AxisXSize - Value - XViewSize + 0.5
         private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
         {
-            XViewPos = e.NewValue + 0.5;
+            if(PPointsCount > 0)
+                XViewPos = AxisXSize - e.NewValue - XViewSize + 0.5;
         }
+        //---
 
-        public void SetXViewSizeBackward(int newSize)
+        public void SetXViewSizeSafely(int newSize)
         {
-            if(newSize > PointsCount)
-                newSize = PointsCount;
-            if (newSize <= 0)
-                newSize = 1;
+            if (PPointsCount > 0)
+            {
+                if (newSize == -1)
+                    newSize = PPointsCount;
+                else if (newSize == 0)
+                    newSize = 1;
+                else if (newSize > PPointsCount)
+                    newSize = PPointsCount;
 
-            int oldSize = XViewSize;
-            int delta = newSize - oldSize;
-            double newPos = XViewPos - delta;
-            if (delta < 0)
-            {
-                XViewSize = newSize;
-                if (newPos < AxisXMin)
-                    XViewPos = AxisXMin;
-                else
-                    XViewPos = newPos;
-            }
-            else
-            {
-                if (newPos < AxisXMin)
-                    XViewPos = AxisXMin;
-                else
-                    XViewPos = newPos;
+                if (XViewPos + newSize > AxisXMax)
+                    XViewPos -= XViewPos + newSize - AxisXMax;
                 XViewSize = newSize;
             }
         }
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            SetXViewSizeBackward(trackBar1.Value);
+            if (PPointsCount > 0)
+            {
+                SetXViewSizeSafely(trackBar1.Value);
+                preferredXViewSize = trackBar1.Value;
+            }
         }
+
+        int preferredXViewSize = -1;
+
+        void UpdateTrackBar()
+        {
+            trackBar1.Value = XViewSize;
+            toolTip.SetToolTip(trackBar1, XViewSize.ToString());
+        }
+
         readonly ToolTip toolTip = new ToolTip();
 
         bool leftflag;
@@ -158,39 +261,50 @@ namespace WindowsFormsApp1
         {
             double start = e.NewSelectionStart;
             double end = e.NewSelectionEnd;
-            leftflag = end < start;
+            leftflag = end > start;
         }
         private void chart1_SelectionRangeChanged(object sender, CursorEventArgs e)
         {
-            if (leftflag)
+            if (PPointsCount > 0)
             {
-                XViewPos = AxisXMin;
-                XViewSize = PointsCount;
-                leftflag = false;
+                if (leftflag)
+                {
+                    XViewPos = AxisXMin;
+                    XViewSize = PPointsCount;
+                    preferredXViewSize = -1;
+                    leftflag = false;
+                }
+                else
+                    preferredXViewSize = XViewSize;
+                UpdateTrackBar();
+                UpdateScrollBar();
+                UpdateTextBox();
+                UpdateYView();
             }
-            hScrollBar1.LargeChange = trackBar1.Value = XViewSize;
-            hScrollBar1.Value = (int)XViewPos;
-            UpdateTextBox();
-            UpdateY2View();
         }
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if('0' <= e.KeyChar && e.KeyChar <= '9')
+            if ('0' <= e.KeyChar && e.KeyChar <= '9')
             {
 
             }
-            else if(e.KeyChar == (char)Keys.Back)
+            else if (e.KeyChar == (char)Keys.Back)
             {
 
             }
-            else if(e.KeyChar == (char)Keys.Enter)
+            else if (e.KeyChar == (char)Keys.Enter)
             {
-                if(int.TryParse(textBox1.Text, out int newSize))
+
+                if (int.TryParse(textBox1.Text, out int newSize))
                 {
-                    SetXViewSizeBackward(newSize);
-                    textBox1.SelectAll();
+                    preferredXViewSize = newSize;
+                    SetXViewSizeSafely(newSize);
                 }
+                else
+                    SetXViewSizeSafely(int.MaxValue);
+                UpdateTextBox();
+                textBox1.SelectAll();
                 e.Handled = true;
             }
             else
@@ -217,30 +331,46 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SetXViewSizeBackward((int)(XViewSize*1.1) + 1);
+            if(PPointsCount > 0)
+            {
+                int newSize = (int)Math.Ceiling(XViewSize * 1.1);
+                if (newSize > PPointsCount)
+                    newSize = PPointsCount;
+                SetXViewSizeSafely(newSize);
+                preferredXViewSize = newSize;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            SetXViewSizeBackward((int)(XViewSize * 0.9));
-            UpdateY2View();
+            if (PPointsCount > 0)
+            {
+                int newSize = (int)(XViewSize * 0.9);
+                if (newSize == 0)
+                    newSize = 1;
+                SetXViewSizeSafely(newSize);
+                preferredXViewSize = newSize;
+            }
         }
 
-        const byte L = 1;
-        const byte H = 0;
-        Axis AxisY2 => ChartArea.AxisY2;
-        AxisScaleView Y2View => AxisY2.ScaleView;
-        IEnumerable<DataPoint> ViewPoints => Points.Skip((int)XViewPos).Take(XViewSize);
+
+        Axis AxisY => ChartArea.AxisY;
+        AxisScaleView YView => AxisY.ScaleView;
+        IEnumerable<DataPoint> ViewPoints => PPoints.Skip((int)XViewPos).Take(XViewSize);
         double ViewPointsMin => ViewPoints.Min(x => x.YValues[L]);
         double ViewPointsMax => ViewPoints.Max(x => x.YValues[H]);
 
-        void UpdateY2View()
+        void UpdateYView()
         {
             double y2viewmin = ViewPointsMin * 0.9;
             double y2viewmax = ViewPointsMax * 1.1;
-            Y2View.Position = y2viewmin;
-            Y2View.Size = y2viewmax - y2viewmin;
+            YView.Position = y2viewmin;
+            YView.Size = y2viewmax - y2viewmin;
         }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine(preferredXViewSize);
+        }
     }
 }
